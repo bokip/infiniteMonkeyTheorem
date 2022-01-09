5d946f72519b440cda9dfeb3d6c1c5eaaf5dc6c4
bokip <bogdanpavlovic@tuta.io>
2022-01-09 10:49:23 +0100

    organized arts

--- art.py
+++ art.py
@@ -1,11 +1,17 @@
 from random import randint
 from outputMessages import *
+from os import mkdir, path
 
 def monkeyArt(width, height, numberOfArts):
     artsGenerated = 0
+    if path.isdir("arts"):
+        info("Direcotry \"arts\" alredy exists")
+    else:
+        mkdir("arts")
+        info("Created directory \"arts\"")
     for i in range(numberOfArts):
         artsGenerated += 1
-        with open(f"art{artsGenerated}.ppm", "w") as art:
+        with open(f"arts/art{artsGenerated}.ppm", "w") as art:
             art.write("P3\n")
             art.write(f"{width} {height}\n")
             art.write("255\n")
