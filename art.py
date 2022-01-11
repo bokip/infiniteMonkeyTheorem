106fc20887759c67af2e1db2a9c827e63774e3cc
bokip <bogdanpavlovic@tuta.io>
2022-01-11 19:04:25 +0100

    added function for black and white

--- art.py
+++ art.py
@@ -23,3 +23,29 @@ def monkeyArt(width, height, numberOfArts):
             ok(f"Successfully generate art number {artsGenerated}!")
     ok(f"{numberOfArts} generated successfully")
 
+def monkeyBlackWhite(width, height, numberOfArts):
+    artsGenerated = 0
+    if path.isdir("arts"):
+        info("Direcotry \"arts\" alredy exists")
+    else:
+        mkdir("arts")
+        info("Created directory \"arts\"")
+    for i in range(numberOfArts):
+        artsGenerated += 1
+        with open(f"arts/art{artsGenerated}.ppm", "w") as art:
+            art.write("P3\n")
+            art.write(f"{width} {height}\n")
+            art.write("255\n")
+            for y in range(height):
+                for x in range(width):
+                    if randint(0,1) == 0:
+                        art.write("255 ")
+                        art.write("255 ")
+                        art.write("255   ")
+                    else:
+                        art.write("0 ")
+                        art.write("0 ")
+                        art.write("   ")
+            ok(f"Successfully generate art number {artsGenerated}!")
+    ok(f"{numberOfArts} generated successfully")
+
